f6344b8fd08ff010bccb6cbdf0d6ebc0d6f949ff
nauhtnn <thuan.nguyenngoc@live.com>
2020-07-14 01:02:43 +0700

    + folder icon

---
+++ MainWindow.xaml.cs
@@ -0,0 +1,111 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Drawing;
+using System.Drawing.Imaging;
+using System.IO;
+using System.Linq;
+using System.Runtime.InteropServices;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace FileExercise
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+
+        private void Button_Click(object sender, RoutedEventArgs e)
+        {
+            // Configure open file dialog box
+            //Microsoft.Win32.OpenFileDialog dlg = new Microsoft.Win32.OpenFileDialog();
+            ////dlg.FileName = "Document"; // Default file name
+            ////dlg.DefaultExt = ".txt"; // Default file extension
+            ////dlg.Filter = "Text documents (.txt)|*.txt"; // Filter files by extension
+
+            //// Show open file dialog box
+            //Nullable<bool> result = dlg.ShowDialog();
+
+            //// Process open file dialog box results
+            //if (result == true)
+            //{
+            //    // Open document
+            //    //output.Text += dlg.FileName;
+            //}
+        }
+
+        private static Icon folderIcon;
+
+        public static Icon FolderLarge => folderIcon ?? (folderIcon = GetStockIcon(SHSIID_FOLDER, SHGSI_SMALLICON));
+
+        private static Icon GetStockIcon(uint type, uint size)
+        {
+            var info = new SHSTOCKICONINFO();
+            info.cbSize = (uint)System.Runtime.InteropServices.Marshal.SizeOf(info);
+
+            SHGetStockIconInfo(type, SHGSI_ICON | size, ref info);
+
+            var icon = (Icon)System.Drawing.Icon.FromHandle(info.hIcon).Clone(); // Get a copy that doesn't use the original handle
+            DestroyIcon(info.hIcon); // Clean up native icon to prevent resource leak
+
+            return icon;
+        }
+
+        [StructLayout(LayoutKind.Sequential, CharSet = CharSet.Unicode)]
+        public struct SHSTOCKICONINFO
+        {
+            public uint cbSize;
+            public IntPtr hIcon;
+            public int iSysIconIndex;
+            public int iIcon;
+            [MarshalAs(UnmanagedType.ByValTStr, SizeConst = 260)]
+            public string szPath;
+        }
+
+        [DllImport("shell32.dll")]
+        public static extern int SHGetStockIconInfo(uint siid, uint uFlags, ref SHSTOCKICONINFO psii);
+
+        [DllImport("user32.dll")]
+        public static extern bool DestroyIcon(IntPtr handle);
+
+        private const uint SHSIID_FOLDER = 0x3;
+        private const uint SHGSI_ICON = 0x100;
+        private const uint SHGSI_LARGEICON = 0x0;
+        private const uint SHGSI_SMALLICON = 0x1;
+
+        private void Grid_Loaded(object sender, RoutedEventArgs e)
+        {
+            var bitmap = FolderLarge.ToBitmap();
+            BitmapImage bitmapImage;
+            using (MemoryStream memory = new MemoryStream())
+            {
+                bitmap.Save(memory, ImageFormat.Png);
+                memory.Position = 0;
+                bitmapImage = new BitmapImage();
+                bitmapImage.BeginInit();
+                bitmapImage.StreamSource = memory;
+                bitmapImage.CacheOption = BitmapCacheOption.OnLoad;
+                bitmapImage.EndInit();
+            }
+            var myImage = new System.Windows.Controls.Image();
+            myImage.Source = bitmapImage;
+            myImage.Width = bitmap.Width;
+            myImage.Height = bitmap.Height;
+            canvas.Children.Add(myImage);
+        }
+    }
+}
