07756f102dddf6c63c712cc41c6e9295e9a347dd
nauhtnn <thuan.nguyenngoc@live.com>
2020-07-14 01:31:49 +0700

    + explorer (tree view)

--- MainWindow.xaml.cs
+++ MainWindow.xaml.cs
@@ -1,10 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
-using System.Drawing;
-using System.Drawing.Imaging;
-using System.IO;
 using System.Linq;
-using System.Runtime.InteropServices;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows;
@@ -12,8 +8,6 @@ using System.Windows.Controls;
 using System.Windows.Data;
 using System.Windows.Documents;
 using System.Windows.Input;
-using System.Windows.Media;
-using System.Windows.Media.Imaging;
 using System.Windows.Navigation;
 using System.Windows.Shapes;
 
@@ -48,64 +42,41 @@ namespace FileExercise
             //}
         }
 
-        private static Icon folderIcon;
-
-        public static Icon FolderLarge => folderIcon ?? (folderIcon = GetStockIcon(SHSIID_FOLDER, SHGSI_SMALLICON));
-
-        private static Icon GetStockIcon(uint type, uint size)
-        {
-            var info = new SHSTOCKICONINFO();
-            info.cbSize = (uint)System.Runtime.InteropServices.Marshal.SizeOf(info);
-
-            SHGetStockIconInfo(type, SHGSI_ICON | size, ref info);
-
-            var icon = (Icon)System.Drawing.Icon.FromHandle(info.hIcon).Clone(); // Get a copy that doesn't use the original handle
-            DestroyIcon(info.hIcon); // Clean up native icon to prevent resource leak
-
-            return icon;
-        }
-
-        [StructLayout(LayoutKind.Sequential, CharSet = CharSet.Unicode)]
-        public struct SHSTOCKICONINFO
-        {
-            public uint cbSize;
-            public IntPtr hIcon;
-            public int iSysIconIndex;
-            public int iIcon;
-            [MarshalAs(UnmanagedType.ByValTStr, SizeConst = 260)]
-            public string szPath;
-        }
-
-        [DllImport("shell32.dll")]
-        public static extern int SHGetStockIconInfo(uint siid, uint uFlags, ref SHSTOCKICONINFO psii);
-
-        [DllImport("user32.dll")]
-        public static extern bool DestroyIcon(IntPtr handle);
-
-        private const uint SHSIID_FOLDER = 0x3;
-        private const uint SHGSI_ICON = 0x100;
-        private const uint SHGSI_LARGEICON = 0x0;
-        private const uint SHGSI_SMALLICON = 0x1;
+        
 
         private void Grid_Loaded(object sender, RoutedEventArgs e)
         {
-            var bitmap = FolderLarge.ToBitmap();
-            BitmapImage bitmapImage;
-            using (MemoryStream memory = new MemoryStream())
+            var explorer = new TreeView();
+            
+            for(int i = 0; i < 10; ++i)
             {
-                bitmap.Save(memory, ImageFormat.Png);
-                memory.Position = 0;
-                bitmapImage = new BitmapImage();
-                bitmapImage.BeginInit();
-                bitmapImage.StreamSource = memory;
-                bitmapImage.CacheOption = BitmapCacheOption.OnLoad;
-                bitmapImage.EndInit();
+                var it = new TreeViewItem();
+                var f = new StackPanel();
+                f.Orientation = Orientation.Horizontal;
+                f.Children.Add(StockIconGetter.GetImage());
+                var text = new TextBlock();
+                text.Text = " hello " + i;
+                f.Children.Add(text);
+                it.Header = f;
+                var pad = new Thickness(4);
+                it.Padding = pad;
+                for (int j = 0; j < 3; ++j)
+                {
+                    var ite = new TreeViewItem();
+                    var folder = new StackPanel();
+                    folder.Orientation = Orientation.Horizontal;
+                    folder.Children.Add(StockIconGetter.GetImage());
+                    var folderName = new TextBlock();
+                    folderName.Text = " hello " + j;
+                    folder.Children.Add(folderName);
+                    ite.Header = folder;
+                    var padding = new Thickness(12);
+                    ite.Padding = padding;
+                    it.Items.Add(ite);
+                }
+                explorer.Items.Add(it);
             }
-            var myImage = new System.Windows.Controls.Image();
-            myImage.Source = bitmapImage;
-            myImage.Width = bitmap.Width;
-            myImage.Height = bitmap.Height;
-            canvas.Children.Add(myImage);
+            canvas.Children.Add(explorer);
         }
     }
 }
---
+++ StockIconGetter.cs
@@ -0,0 +1,78 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Runtime.InteropServices;
+using System.Drawing;
+using System.Drawing.Imaging;
+using System.IO;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+
+namespace FileExercise
+{
+    class StockIconGetter
+    {
+        private static Icon folderIcon;
+
+        private static Icon FolderLarge => folderIcon ?? (folderIcon = GetStockIcon(SHSIID_FOLDER, SHGSI_SMALLICON));
+
+        private static Icon GetStockIcon(uint type, uint size)
+        {
+            var info = new SHSTOCKICONINFO();
+            info.cbSize = (uint)System.Runtime.InteropServices.Marshal.SizeOf(info);
+
+            SHGetStockIconInfo(type, SHGSI_ICON | size, ref info);
+
+            var icon = (Icon)System.Drawing.Icon.FromHandle(info.hIcon).Clone(); // Get a copy that doesn't use the original handle
+            DestroyIcon(info.hIcon); // Clean up native icon to prevent resource leak
+
+            return icon;
+        }
+
+        public static System.Windows.Controls.Image GetImage()
+        {
+            var bitmap = FolderLarge.ToBitmap();
+            BitmapImage bitmapImage;
+            using (MemoryStream memory = new MemoryStream())
+            {
+                bitmap.Save(memory, ImageFormat.Png);
+                memory.Position = 0;
+                bitmapImage = new BitmapImage();
+                bitmapImage.BeginInit();
+                bitmapImage.StreamSource = memory;
+                bitmapImage.CacheOption = BitmapCacheOption.OnLoad;
+                bitmapImage.EndInit();
+            }
+            var myImage = new System.Windows.Controls.Image();
+            myImage.Source = bitmapImage;
+            myImage.Width = bitmap.Width;
+            myImage.Height = bitmap.Height;
+
+            return myImage;
+        }
+
+        [StructLayout(LayoutKind.Sequential, CharSet = CharSet.Unicode)]
+        public struct SHSTOCKICONINFO
+        {
+            public uint cbSize;
+            public IntPtr hIcon;
+            public int iSysIconIndex;
+            public int iIcon;
+            [MarshalAs(UnmanagedType.ByValTStr, SizeConst = 260)]
+            public string szPath;
+        }
+
+        [DllImport("shell32.dll")]
+        public static extern int SHGetStockIconInfo(uint siid, uint uFlags, ref SHSTOCKICONINFO psii);
+
+        [DllImport("user32.dll")]
+        public static extern bool DestroyIcon(IntPtr handle);
+
+        private const uint SHSIID_FOLDER = 0x3;
+        private const uint SHGSI_ICON = 0x100;
+        private const uint SHGSI_LARGEICON = 0x0;
+        private const uint SHGSI_SMALLICON = 0x1;
+    }
+}
